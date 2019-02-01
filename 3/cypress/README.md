f7513ec36c481ea8e9a6d6b9af72533f2749be52
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-01-31 22:01:12 -0500

    copying

---
+++ 3/cypress/README.md
@@ -0,0 +1,19 @@
+# Cypress.io end-to-end tests
+
+[Cypress.io](https://www.cypress.io) is an open source, MIT licensed end-to-end test runner
+
+- [https://github.com/cypress.io/cypress](https://github.com/cypress.io/cypress)
+- [https://docs.cypress.io/](https://docs.cypress.io/)
+
+## Folder structure
+
+These folders holds end-to-end tests and supporting files for Cypress Test Runner.
+
+- [fixtures](fixtures) holds optional JSON data for mocking, [read more](https://on.cypress.io/fixture)
+- [integration](integration) holds actual test files, [read more](https://on.cypress.io/writing-and-organizing-tests)
+- [plugins](plugins) allow you to customize how tests are loaded, [read more](https://on.cypress.io/plugins)
+- [support](support) file runs before all tests and is a great place to write or load additional custom commands, [read more](https://on.cypress.io/writing-and-organizing-tests#Support-file)
+
+## `cypress.json` file
+
+You can configure project options in the [../cypress.json](../cypress.json) file, see [Cypress configuration doc](https://on.cypress.io/configuration).
--- src/index.js
+++ src/index.js
@@ -2,16 +2,41 @@
 
 'use strict'
 
+const shell = require('shelljs')
+const debug = require('debug')('@bahmutov/cly')
+const path = require('path')
+
 const noOptions = () => {}
 
 const initCommand = () => {
   console.log('scaffolding new Cypress project')
+  debug('working directory %s', process.cwd())
+
+  if (shell.test('-d', 'cypress')) {
+    console.error('Found existing folder "cypress", cannot scaffold')
+    process.exit(1)
+  }
+  if (shell.test('-f', 'cypress.json')) {
+    console.error('Found existing file "cypress.json", cannot scaffold')
+    process.exit(1)
+  }
+
+  // TODO find Cypress version from package.json
+  const cypressVersion = '3'
+  const sourceFolder = path.join(__dirname, '..', cypressVersion)
+
+  shell.cp(path.join(sourceFolder, 'cypress.json'), process.cwd())
+  shell.cp('-r', path.join(sourceFolder, 'cypress'), process.cwd())
 }
 
 // eslint-disable-next-line no-unused-expressions
-require('yargs').command(
-  ['init', '$0'],
-  'scaffold Cypress tests',
-  noOptions,
-  initCommand
-).argv
+require('yargs')
+  .command(
+    ['init', 'scaffold'],
+    'scaffold Cypress tests',
+    noOptions,
+    initCommand
+  )
+  .demandCommand()
+  .help()
+  .wrap(72).argv
